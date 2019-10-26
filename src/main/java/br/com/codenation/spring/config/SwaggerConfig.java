e1568ba859fe732dc66b0d9939c6115897b64de4
Marcella Farias <marcellafc@live.com>
2019-10-26 11:05:13 -0300

    Add config swagger

--- src/main/java/br/com/codenation/spring/config/SwaggerConfig.java
+++ src/main/java/br/com/codenation/spring/config/SwaggerConfig.java
@@ -2,6 +2,12 @@ package br.com.codenation.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -9,7 +15,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    public Docket
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(
+                        "br.com.codenation.spring.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
 
-
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Documentação da API Rest")
+                .description("\" Documentação do projeto que estamos desenvolvendo\"")
+                .version("1.0.0")
+                .contact(new Contact("Marcella Farias", "https://github.com/marcellafarias", "fcosta.marcella@gmail.com"))
+                .build();
+    }
 }
